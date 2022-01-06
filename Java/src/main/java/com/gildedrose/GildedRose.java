5b82699683d766517b8f084d61d25f8c8ae7822c
red1 <redouaneelalami@gmail.com>
2022-01-06 12:40:40 +0100

    feat : solution

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
 class GildedRose {
+
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -8,55 +12,30 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+    private void updateItemQuality(Item item) {
+        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE)) {
+            if (!item.name.equals(SULFURAS)) {
+                item.adjustQuality(-1);
             }
+        } else {
+            item.adjustQuality(1);
+            updateBackStage(item);
+        }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+        item.updateSellInForRegularItems();
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+        item.updateQualityForOutDatedItem();
+    }
+
+    private void updateBackStage(Item item) {
+        if (item.name.equals(BACKSTAGE)) {
+            if (item.sellIn < 11) item.adjustQuality(1);
+            if (item.sellIn < 6) item.adjustQuality(1);
         }
     }
 }
--- Java/src/main/java/com/gildedrose/Item.java
+++ Java/src/main/java/com/gildedrose/Item.java
@@ -1,7 +1,8 @@
 package com.gildedrose;
 
-public class Item {
+import java.util.Objects;
 
+public class Item {
     public String name;
 
     public int sellIn;
@@ -14,8 +15,43 @@ public class Item {
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return sellIn == item.sellIn && quality == item.quality && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellIn, quality);
+    }
+
+    void adjustQuality(int adjustment) {
+        int newQuality = quality + adjustment;
+        boolean isInRange = newQuality <= 50 && newQuality > 0;
+        if (isInRange) quality = newQuality;
+    }
+
+    void updateSellInForRegularItems() {
+        if (!name.equals(GildedRose.SULFURAS)) sellIn = sellIn - 1;
+    }
+
+    void updateQualityForOutDatedItem() {
+        if (sellIn < 0) {
+            if (!name.equals(GildedRose.AGED_BRIE)) {
+                if (!name.equals(GildedRose.BACKSTAGE)) {
+                    if (!name.equals(GildedRose.SULFURAS)) {
+                        adjustQuality(-1);
+                    }
+                } else quality = 0;
+            } else adjustQuality(1);
+        }
     }
 }
